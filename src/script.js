262a49621e628ab4496cdf8cede5140704a1cafe
KiraniaKharisa <kiraniakharisasuyatno@gmail.com>
2024-10-26 12:58:54 +0700

    kirania first commit

--- src/script.js
+++ src/script.js
@@ -1,14 +1,23 @@
 const navbar = document.querySelector(".navbar"),
-allSEction = document.querySelectorAll("section"),
+allSection = document.querySelectorAll("section"),
 allLink = document.querySelectorAll(".navbar .menu a");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 50) {
-        navbar.classList.add("discroll");
-    } else if(window.scrollY < 50) {
-        navbar.classList.remove("discroll");
+    allSection.forEach(section => {
+        let scrollWindow = window.scrollY,
+        sectionOffset = section.offsetTop - 100,
+        sectionHeight = section.offsetHeight,
+        idSection = section.getAttribute('id');
 
-    }
+        if(scrollWindow >= sectionOffset && scrollWindow < sectionOffset + sectionHeight) {
+            allLink.forEach(link => {
+                link.classList.remove("active");
+                document.querySelector(".navbar .menu a[href*=" + idSection + "]").classList.add("active");
+            })
+        }
+    })
+
+    navbar.classList.toggle("discroll", window.scrollY > 50);
 })
 
 document.querySelector(".hamburger").addEventListener("click", () => {
